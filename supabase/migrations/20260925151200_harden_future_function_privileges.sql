7ca65153e6c3ecf0855ee19124ce95aaa4142a0a
AgroBrokerIA <sequeirasamanta15@gmail.com>
2026-09-25 11:50:38 -0300

    chore: version signature security infrastructure

---
+++ supabase/migrations/20260925151200_harden_future_function_privileges.sql
@@ -0,0 +1,1 @@
+alter default privileges in schema public revoke execute on functions from public; alter default privileges in schema public revoke execute on functions from anon; alter default privileges in schema public revoke execute on functions from authenticated;
